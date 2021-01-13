6d07b46c7b8a74e33081610ceeabb99b586050d7
artem_nanavov <artemnanavov@gmail.com>
2021-01-13 19:53:10 +0300

    Updates

---
+++ precache-manifest.2f43c03fe5010f13b13d65ad88c9373c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62aa82dcac47f27fd7f8d817950ec556",
+    "url": "/fly/index.html"
+  },
+  {
+    "revision": "99551cb34f6b37092a0d",
+    "url": "/fly/static/css/main.5d7dc873.chunk.css"
+  },
+  {
+    "revision": "cafe4d8db2de4ef6a2d7",
+    "url": "/fly/static/js/2.989defb0.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/fly/static/js/2.989defb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99551cb34f6b37092a0d",
+    "url": "/fly/static/js/main.a09dfc34.chunk.js"
+  },
+  {
+    "revision": "4b8c346b2c3a557d1200",
+    "url": "/fly/static/js/runtime-main.456b3e06.js"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/fly/static/media/Logo.0c089234.svg"
+  }
+]);
